b878a85c71f1ab66034eec92dcd04f4d776602e3
ngleav <nathan.gleaves@gmail.com>
2013-09-22 21:09:30 -0500

    adding to board

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,8 +1,18 @@
+import java.util.BitSet;
+
 /**
  * User: ngleav
  * Date: 9/22/13
  * Time: 8:24 PM
  * To change this template use File | Settings | File Templates.
  */
-public class Board {
+
+public class Board extends BitSet{
+
+    public Board() {
+
+    }
+    public Board(int hole) {
+
+    }
 }
